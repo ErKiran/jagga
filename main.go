2e5c3181eb294171f94e63c4c8397bc98cbffbf0
ErKiran <kiruu1238@gmail.com>
2020-08-02 20:01:59 +0545

    WIP

--- main.go
+++ main.go
@@ -6,5 +6,7 @@ import (
 
 
 func main(){
-	 utils.HillBase(360,14,5,2,utils.INTERNATIONALSYSTEM)
+	 utils.HillBase(360,0,0,0,utils.TERAISYSTEM)
+	//  utils.TeraiBase(15,20,4,utils.INTERNATIONALSYSTEM)
+	 utils.TeraiBase(27,0,17.20791494388648,utils.HILLYSYSTEM)
 }
--- utils/conversions.go
+++ utils/conversions.go
@@ -6,22 +6,55 @@ import (
 
 func HillBase(ropani float64, aana float64, paisa float64, daam float64, convertTo string) {
 
-	totalInSqMeter := ropani*ROPANI + aana*AANA + paisa*PAISA + daam*DAAM
+	totalAreaInSqMeter := ropani*ROPANI + aana*AANA + paisa*PAISA + daam*DAAM
 
 	if convertTo == TERAISYSTEM {
-		res := TeraiSystem(totalInSqMeter)
+		res := TeraiSystem(totalAreaInSqMeter)
 		Pretty("res", res)
 	}
 
 	if convertTo == INTERNATIONALSYSTEM {
-		res := InternationalSystem(totalInSqMeter)
+		res := InternationalSystem(totalAreaInSqMeter)
 		Pretty("res", res)
 	}
 
 }
 
+func TeraiBase(bigha float64, katha float64, dhur float64, convertTo string) {
+	totalAreaInSqMeter := bigha*BIGHA + katha*KATHA + dhur*DHUR
+
+	if convertTo == INTERNATIONALSYSTEM {
+		res := InternationalSystem(totalAreaInSqMeter)
+		Pretty("res", res)
+	}
+
+	if convertTo == HILLYSYSTEM {
+		res := HillySystem(totalAreaInSqMeter)
+		Pretty("res", res)
+	}
+}
+
 func InternationalSystem(area float64) (result International) {
 	return International{SQUAREMETER: area, SQUAREFOOT: area / SQUAREFOOT, HECTARE: area / HECTARE, ACRE: area / ACRE}
+}
+
+func HillySystem(area float64) (result Hill) {
+	if area/ROPANI >= 1 {
+		remainderAnna := math.Mod(area, ROPANI)
+		if remainderAnna/AANA >= 1 {
+			remainderPaisa := math.Mod(area, AANA)
+			if remainderPaisa/PAISA >= 1 {
+				remainderDaam := math.Mod(area, DAAM)
+				res := Hill{ROPANI: math.Floor(area / ROPANI), AANA: math.Floor(remainderAnna / AANA), PAISA: math.Floor(remainderPaisa / PAISA), DAAM: remainderDaam / DAAM}
+				return res
+			}
+			res := Hill{ROPANI: math.Floor(area / ROPANI), AANA: math.Floor(remainderAnna / AANA), DAAM: remainderPaisa / DAAM}
+			return res
+		}
+		return
+	}
+
+	return
 }
 
 func TeraiSystem(area float64) (result Terai) {
